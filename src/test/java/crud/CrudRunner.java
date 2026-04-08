35bca563f958155d9e338f330b5c76cda0557bec
tutoOmar <omar-ortiz@javeriana.edu.co>
2026-04-08 18:39:05 -0500

    Refactor Karate tests: remove old examples and add CRUD operations for user management

--- src/test/java/crud/CrudRunner.java
+++ src/test/java/crud/CrudRunner.java
@@ -1,16 +1,15 @@
-package examples;
+package crud;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-class ExamplesTest {
+class CrudRunner {
 
     @Test
     void testParallel() {
-        Results results = Runner.path("classpath:examples")
-                //.outputCucumberJson(true)
+        Results results = Runner.path("classpath:crud")
                 .parallel(5);
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
